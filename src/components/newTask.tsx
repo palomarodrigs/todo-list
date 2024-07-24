71ab9f092a1ac1eb723c7414ddd0bf0fb07be68a
palomarodrigs <palomarodrigues.0013@gmail.com>
2024-07-24 11:43:54 -0300

    feat: add prop onAddNewTask and add handleCreateTask to NewTask component

--- src/components/newTask.tsx
+++ src/components/newTask.tsx
@@ -1,13 +1,32 @@
+import { useState } from 'react'
 import styles from './newTask.module.css'
 
 import Plus from '../assets/plus.svg'
 
-export function NewTask() {
+interface NewTaskProps {
+  onAddNewTask: (content: string) => void
+}
+
+export function NewTask({ onAddNewTask }: NewTaskProps) {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleCreateTask = () => {
+    if (inputValue.trim()) {
+      onAddNewTask(inputValue)
+      setInputValue('')
+    }
+  }
+
   return (
     <div className={styles.wrapper}>
-      <input type="text" placeholder="Adicione uma tarefa" />
+      <input
+        type="text"
+        placeholder="Adicione uma tarefa"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
 
-      <button className={styles.newTaskButton}>
+      <button className={styles.newTaskButton} onClick={handleCreateTask}>
         <p>Criar</p>
         <img src={Plus} />
       </button>
