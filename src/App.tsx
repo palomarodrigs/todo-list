9d3304c45aecf8c035a2a06c4da854f21972ba7e
palomarodrigs <palomarodrigues.0013@gmail.com>
2024-07-24 11:49:24 -0300

    feat: add handleAddNewTask, get storedTasks and tasks  length to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,56 @@
 import './App.css'
-import { Counter } from './components/counter'
+
+import { useState, useEffect } from 'react'
 
 import { Header } from './components/header'
 import { NewTask } from './components/newTask'
+import { Counter } from './components/counter'
+
+interface ITask {
+  id: number
+  content: string
+  isChecked: boolean
+}
 
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  const handleAddNewTask = (content: string) => {
+    const newTask: ITask = {
+      id: Date.now(),
+      content,
+      isChecked: false,
+    }
+
+    localStorage.setItem('@todo:tasks', JSON.stringify([...tasks, newTask]))
+    setTasks([...tasks, newTask])
+  }
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('@todo:tasks')
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks))
+    }
+  }, [])
+
   return (
     <main>
       <Header />
 
       <section>
-        <NewTask />
+        <NewTask onAddNewTask={handleAddNewTask} />
 
         <div className="info">
           <div className="created-tasks">
             <p>Tarefas criadas</p>
 
-            <Counter count={0} />
+            <Counter count={tasks.length} />
           </div>
 
           <div className="concluded-tasks">
             <p>Concluídas</p>
 
-            <Counter count={0} />
+            <Counter count={tasks.filter((task) => task.isChecked).length} />
           </div>
         </div>
       </section>
